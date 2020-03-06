7e9e2f043d0a210afcf9561a3e0152e4a3d10411
Matts966 <Matts966@users.noreply.github.com>
2020-03-07 00:08:54 +0900

    Start new gatsby site

---
+++ src/pages/index.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react'
+import { Link } from 'gatsby'
+
+import Page from '../components/Page'
+import Container from '../components/Container'
+import IndexLayout from '../layouts'
+
+const IndexPage = () => (
+  <IndexLayout>
+    <Page>
+      <Container>
+        <h1>Matts966.github.io</h1>
+        <p>Welcome to my Gatsby site.</p>
+        <Link to="/slides/">my slides</Link>
+      </Container>
+    </Page>
+  </IndexLayout>
+)
+
+export default IndexPage
